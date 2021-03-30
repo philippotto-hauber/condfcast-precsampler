18d56b7f33eb03087b22312cf9b28483d1b74e14
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-03-30 09:02:59 +0200

    update HS sim smoother
    - check restrictions on the permuted, not original draw. This saves the
    computational cost of having to reverse the permutation for each
    candidate. However, this requires that the upper and lower bounds of the
    restrictions need to be permuted once before drawing the candidates.

--- precsampler/simsmooth_HS.m
+++ precsampler/simsmooth_HS.m
@@ -44,12 +44,13 @@ end
 [Nn, Nt] = size(Y_o); % # of observations
 Nh = size(Y_f, 2); % forecast horizon
 NtNh = Nt + Nh; % # of total periods
-Nr = size(params.phi, 1); % # of states
+Ns = size(params.phi, 1); % # of states
 
 % vectorized yobs, removing missings
 y = vec([Y_o, Y_f]); 
 yobs = y(~isnan(y),1); 
 Nmis = sum(isnan(y));
+Nobs = sum(~isnan(y));
 
 % precision matrix Q
 [PQP_fymis, PQP_fymis_yobs] = construct_PQP(params, NtNh, Nmis, p_z);
@@ -59,32 +60,33 @@ chol_PQP_fymis = chol(PQP_fymis, 'lower');
 b_fymis = rue_held_alg2_1(chol_PQP_fymis, -PQP_fymis_yobs * yobs);
 if strcmp(ftype, 'conditional (soft)')    
     % vectorized restrictions
-    y_l = vec([NaN(size(Y_o)), Y_l]);
-    ind_l = ~isnan(y_l);
-    y_u = vec([NaN(size(Y_o)), Y_u]);
-    ind_u = ~isnan(y_u);
+    y_l = [NaN(Ns*NtNh, 1); vec([NaN(size(Y_o)), Y_l])];
+    y_l_perm = y_l(p_z);
+    y_l_perm = y_l_perm(1:end-Nobs);
+    ind_l = ~isnan(y_l_perm);
+    y_u = [NaN(Ns*NtNh, 1); vec([NaN(size(Y_o)), Y_u])];
+    y_u_perm = y_u(p_z); 
+    y_u_perm = y_u_perm(1:end-Nobs);
+    ind_u = ~isnan(y_u_perm);
     % draw proposals
     condition_satisfied = false;
     z_draw = NaN(size(b_fymis, 1), 1);
     while not(condition_satisfied)
         % draw candidate
         fxmis_draw = rue_held_alg2_4(chol_PQP_fymis, b_fymis);
-        % reverse permutation and back out y to be able to check conditions
-        z_draw(p_z,1) = [fxmis_draw; yobs];
-        y_draw = z_draw(NtNh*Nr+1:end, 1);  
         % check soft conditions
-        if all(y_draw(ind_l, 1) > y_l(ind_l, 1)) && all(y_draw(ind_u, 1) < y_u(ind_u, 1))
+        if all(fxmis_draw(ind_l, 1) > y_l_perm(ind_l, 1)) && all(fxmis_draw(ind_u, 1) < y_u_perm(ind_u, 1))
             condition_satisfied = true;
         end
     end
 else
-    fxmis_draw = rue_held_alg2_4(chol_PQP_fymis, b_fymis); % reverse permutation
-    z_draw(p_z,:) = [fxmis_draw; repmat(yobs, 1)];
+    fxmis_draw = rue_held_alg2_4(chol_PQP_fymis, b_fymis); % draw joint     
 end
 
-% reshape draw of z 
-fdraw = reshape(z_draw(1:NtNh*Nr, :), Nr, NtNh); 
-Ydraw = reshape(z_draw(NtNh*Nr+1:end, :), Nn, NtNh);
+% reshape draw of z and back out return args
+z_draw(p_z, :) = [fxmis_draw; repmat(yobs, 1)]; % reverse permutation => z = [vec(f); vec([Y_o, Y_f])]!
+fdraw = reshape(z_draw(1:NtNh*Ns, :), Ns, NtNh); 
+Ydraw = reshape(z_draw(NtNh*Ns+1:end, :), Nn, NtNh);
 if ~strcmp(ftype, 'none') % only return forecasts
     Ydraw = Ydraw(:, Nt+1:NtNh);
 end
