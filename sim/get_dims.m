495747fc1df45b86ce840f695466c335335f9042
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-04-04 16:48:57 +0200

    add func to manage dims of sim

---
+++ sim/get_dims.m
@@ -0,0 +1,42 @@
+function [dims, dims_str] = get_dims(n)
+
+switch(n)
+    case 1
+        dims.Nt = 100;
+        dims.Nh = 20; 
+        dims.Nn = 20;
+        dims.Ns = 2;
+        dims_str = ['Nt_', num2str(dims.Nt), ...
+                   '_Nh_', num2str(dims.Nh), ...
+                   '_Nn_', num2str(dims.Nn), ...
+                   '_Ns_', num2str(dims.Ns)];
+   case 2
+        dims.Nt = 100;
+        dims.Nh = 20; 
+        dims.Nn = 100;
+        dims.Ns = 2;
+        dims_str = ['Nt_', num2str(dims.Nt), ...
+                   '_Nh_', num2str(dims.Nh), ...
+                   '_Nn_', num2str(dims.Nn), ...
+                   '_Ns_', num2str(dims.Ns)];
+  case 3
+        dims.Nt = 100;
+        dims.Nh = 20; 
+        dims.Nn = 100;
+        dims.Ns = 20;
+        dims_str = ['Nt_', num2str(dims.Nt), ...
+                   '_Nh_', num2str(dims.Nh), ...
+                   '_Nn_', num2str(dims.Nn), ...
+                   '_Ns_', num2str(dims.Ns)];
+   case 4
+        dims.Nt = 100;
+        dims.Nh = 20; 
+        dims.Nn = 20;
+        dims.Ns = 25;
+        dims_str = ['Nt_', num2str(dims.Nt), ...
+                   '_Nh_', num2str(dims.Nh), ...
+                   '_Nn_', num2str(dims.Nn), ...
+                   '_Ns_', num2str(dims.Ns)];
+end
+
+
