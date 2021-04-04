cb913be30767997ce8277ac2f296a65227f97867
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-04-04 11:18:17 +0200

    dims struc, ssm and var

--- sim/DK2002/test_DK2002.m
+++ sim/DK2002/test_DK2002.m
@@ -17,46 +17,46 @@ addpath('../../sim/') % path to generate_data.m
 
 rng(1234) % set random seed for reproducibility
 
-Nt = 50; % # of in-sample observations
-Nn = 10; % # of variables
-Ns = 1; % # of states
 Nm = 10; % # of draws
 max_iter = 10000; % maxmimum number of candidates per parameter draw
 
+model = 'ssm'; 
+dims.Nt = 50; % # of in-sample observations
+dims.Nn = 10; % # of variables
+if strcmp(model, 'var')
+elseif strcmp(model, 'ssm')
+dims.Ns = 1; % # of states
+end
+
+
 % type of forecast
-ftype = 'conditional (soft)'; % {'none', 'unconditional', 'conditional (hard)', 'conditional (soft)'} 
+ftype = 'conditional (hard)'; % {'none', 'unconditional', 'conditional (hard)', 'conditional (soft)'} 
 
 % forecast horizon
 if strcmp(ftype, 'none')
-    Nh = 0; 
+    dims.Nh = 0; 
 else
-    Nh = 20;
+    dims.Nh = 20;
 end
 
 
 %% simulate data, state space params
-simdata = generate_data(Nt, Nh, Nn, Ns);
+simdata = generate_data(dims, model);
 
 % observables
 Y_o = simdata.y; 
 
 % state space params
-T = simdata.params.phi; 
-Z = simdata.params.lambda;
-H = diag(simdata.params.sig_eps);
-Q = simdata.params.sig_ups; 
-R = eye(Ns); 
-s0 = zeros(size(T, 1), 1); 
-P0 = 1 * eye(size(T, 1));
+[T, Z, H, R, Q, s0, P0] = get_statespaceparams(simdata, model);
 
 %% no forecasting => standard simulation smoothing
 
 % construct input args Y_f, Y_l, Y_u
 if strcmp(ftype, 'none')
-    Y_f = NaN(Nn, Nh);
+    Y_f = NaN(dims.Nn, dims.Nh);
     Y_u = [];
     Y_l = [];
-    adraw = NaN(Ns, Nt, Nm);
+    adraw = NaN(dims.Ns, dims.Nt, Nm);
     tic
     for m = 1:Nm
         [adraw(:, :, m), ~] = simsmooth_DK(Y_o, Y_f, Y_u, Y_l, T, Z, H, R, Q, s0, P0, max_iter);
@@ -67,13 +67,13 @@ end
 %% unconditional forecasting
 % construct input args Y_f, Y_l, Y_u
 if strcmp(ftype, 'unconditional')
-    Y_f = NaN(Nn, Nh);
+    Y_f = NaN(dims.Nn, dims.Nh);
     Y_u = [];
     Y_l = [];
     
     % CK sim smoother
-    adraw = NaN(Ns, Nt+Nh, Nm);
-    Ydraw = NaN(Nn, Nh, Nm);
+    adraw = NaN(dims.Ns, dims.Nt+dims.Nh, Nm);
+    Ydraw = NaN(dims.Nn, dims.Nh, Nm);
 
     tic
     for m = 1:Nm
@@ -85,15 +85,15 @@ end
 %% conditional forecasting (hard)
 
 if strcmp(ftype, 'conditional (hard)')
-    Y_f = NaN(Nn, Nh);
+    Y_f = NaN(dims.Nn, dims.Nh);
     Y_f(1, :) = simdata.yfore(1, :);
-    Y_f(2, 1:Nh/2) = simdata.yfore(2, 1:Nh/2);
+    Y_f(2, 1:dims.Nh/2) = simdata.yfore(2, 1:dims.Nh/2);
     Y_u = [];
     Y_l = [];
     
     % DK sim smoother
-    adraw = NaN(Ns, Nt+Nh, Nm);
-    Ydraw = NaN(Nn, Nh, Nm);
+    adraw = NaN(dims.Ns, dims.Nt+dims.Nh, Nm);
+    Ydraw = NaN(dims.Nn, dims.Nh, Nm);
 
     tic
     for m = 1:Nm
@@ -105,16 +105,16 @@ end
 %% conditional forecasting (soft)
 
 if strcmp(ftype, 'conditional (soft)')
-    Y_f = NaN(Nn, Nh); 
+    Y_f = NaN(dims.Nn, dims.Nh); 
     sig = sqrt(var(simdata.y, [], 2));
-    Y_l = NaN(Nn, Nh);
-    Y_l(1:Nn/10,1:Nh) = simdata.yfore(1:Nn/10,1:Nh) - repmat(3 * sig(1:Nn/10, 1), 1, Nh);
-    Y_u = NaN(Nn, Nh);
-    Y_u(1:Nn/10,:) = 0; 
+    Y_l = NaN(dims.Nn, dims.Nh);
+    Y_l(1:dims.Nn/10,1:dims.Nh) = simdata.yfore(1:dims.Nn/10,1:dims.Nh) - repmat(3 * sig(1:dims.Nn/10, 1), 1, dims.Nh);
+    Y_u = NaN(dims.Nn, dims.Nh);
+    Y_u(1:dims.Nn/10,:) = 0; 
     
     % DK sim smoother
-    adraw = NaN(Ns, Nt+Nh, Nm);
-    Ydraw = NaN(Nn, Nh, Nm);
+    adraw = NaN(dims.Ns, dims.Nt+dims.Nh, Nm);
+    Ydraw = NaN(dims.Nn, dims.Nh, Nm);
 
     tic
     for m = 1:Nm
@@ -151,7 +151,7 @@ if ~strcmp(ftype, 'none')
     plot([simdata.y(ind_n, :), quantile(Ydraw(ind_n, :, :), 0.9, 3)], 'b:')
     title([ftype, ', y_{' num2str(ind_n) '}'])
     subplot(3,1,3)
-    ind_n = Nn; 
+    ind_n = dims.Nn; 
     plot([simdata.y(ind_n, :) simdata.yfore(ind_n, :)]', 'k-')
     hold on
     plot([simdata.y(ind_n, :), quantile(Ydraw(ind_n, :, :), 0.5, 3)], 'b-')
