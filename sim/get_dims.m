b225c0ce8d81aa58ee4e3c92ff4790bd140e385c
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-04-04 23:00:56 +0200

    lower Nh to 5
    with previous value of 20 too many candidate draws were being rejected

--- sim/get_dims.m
+++ sim/get_dims.m
@@ -3,7 +3,7 @@ function [dims, dims_str] = get_dims(n)
 switch(n)
     case 1
         dims.Nt = 100;
-        dims.Nh = 20; 
+        dims.Nh = 5; 
         dims.Nn = 20;
         dims.Ns = 2;
         dims_str = ['Nt_', num2str(dims.Nt), ...
@@ -12,7 +12,7 @@ switch(n)
                    '_Ns_', num2str(dims.Ns)];
    case 2
         dims.Nt = 100;
-        dims.Nh = 20; 
+        dims.Nh = 5; 
         dims.Nn = 100;
         dims.Ns = 2;
         dims_str = ['Nt_', num2str(dims.Nt), ...
@@ -21,7 +21,7 @@ switch(n)
                    '_Ns_', num2str(dims.Ns)];
   case 3
         dims.Nt = 100;
-        dims.Nh = 20; 
+        dims.Nh = 5; 
         dims.Nn = 100;
         dims.Ns = 20;
         dims_str = ['Nt_', num2str(dims.Nt), ...
@@ -30,7 +30,7 @@ switch(n)
                    '_Ns_', num2str(dims.Ns)];
    case 4
         dims.Nt = 100;
-        dims.Nh = 20; 
+        dims.Nh = 5; 
         dims.Nn = 20;
         dims.Ns = 25;
         dims_str = ['Nt_', num2str(dims.Nt), ...
