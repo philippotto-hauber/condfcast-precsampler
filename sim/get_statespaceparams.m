5e75ce7495a9c5e3e2e11ef4e6b8a361217cff47
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-04-10 06:22:01 +0200

    state space params for VAR

--- sim/get_statespaceparams.m
+++ sim/get_statespaceparams.m
@@ -1,22 +1,28 @@
-function [T, Z, H, R, Q, s0, P0] = get_statespaceparams(simdata, model)
+function [T, Z, H, R, Q, s0, P0] = get_statespaceparams(params, y, model)
 
 if strcmp(model, 'ssm')
     % generic state space model
-    T = simdata.params.phi; 
-    Z = simdata.params.lambda;
-    H = diag(simdata.params.sig_eps);
-    Q = simdata.params.sig_ups; 
+    T = params.params.phi; 
+    Z = params.params.lambda;
+    H = diag(params.params.sig_eps);
+    Q = params.params.sig_ups; 
     R = eye(size(T, 1)); 
     s0 = zeros(size(T, 1), 1); 
     P0 = 1 * eye(size(T, 1));
 elseif strcmp(model, 'var')
     % vector autoregression
-    [T, R, Q] = var_companion(simdata.params.B, simdata.params.Sigma);
+    [T, R, Q] = var_companion(params.B, params.Sigma);
     Z = zeros(size(Q, 1), size(T, 2));
     Z(1:size(Q, 1), 1: size(Q, 1)) = eye(size(Q, 1));
     H = zeros(size(Q, 1)); % 1e-8 * eye(size(Q, 1))
-    s0 = zeros(size(T, 1), 1); % FIX LATER
-    P0 = zeros(size(T, 1)); % 1e-8 * eye(size(T, 1))
+    
+    % s0 and P0
+    Np = size(params.B, 2) / size(params.B, 1);
+    s0 = y(:, end); 
+    for p = 1:Np-1
+        s0 = [s0; y(:, end-p)]; 
+    end
+    P0 = zeros(size(s0, 1)); % 1e-8 * eye(size(T, 1))
 end
    
 
