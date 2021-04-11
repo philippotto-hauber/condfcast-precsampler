f73f283e471042bd25a447b23307754c114b1592
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-04-11 10:57:56 +0200

    update timesamplers.m
    - pass simdata.y to get_statespaceparams.m so that s0 can be constructed
    if Y_o = [] as for VARs
    
    - case distinction for permutation index p_z as the number of states is
    0 for VARs

--- sim/timesamplers.m
+++ sim/timesamplers.m
@@ -4,22 +4,26 @@ max_iter = 1e5;
 if strcmp(sampler, 'CK')    
     tic;
     for m = 1:Nm
-        [T, Z, H, R, Q, s0, P0] = get_statespaceparams(simdata, model);
+        [T, Z, H, R, Q, s0, P0] = get_statespaceparams(simdata.params, simdata.y, model);
         [sdraw, Ydraw] = simsmooth_CK(Y_o, Y_f, Y_u, Y_l, T, Z, H, R, Q, s0, P0, max_iter);
     end
     telapsed = toc; 
 elseif strcmp(sampler, 'DK')
     tic;
     for m = 1:Nm
-        [T, Z, H, R, Q, s0, P0] = get_statespaceparams(simdata, model);
-        [sdraw, Ydraw] = simsmooth_CK(Y_o, Y_f, Y_u, Y_l, T, Z, H, R, Q, s0, P0, max_iter);
+        [T, Z, H, R, Q, s0, P0] = get_statespaceparams(simdata.params, simdata.y, model);
+        [sdraw, Ydraw] = simsmooth_DK(Y_o, Y_f, Y_u, Y_l, T, Z, H, R, Q, s0, P0, max_iter);
     end
     telapsed = toc; 
 elseif strcmp(sampler, 'HS')
     tic;
-    p_z = p_timet([Y_o, Y_f], size(simdata.aalpha, 1));
+    if strcmp(model, 'var') % no states!
+        p_z = p_timet([Y_o, Y_f], 0);
+    else % account for states when permuting by time-t
+        p_z = p_timet([Y_o, Y_f], size(simdata.aalpha, 1));
+    end
     for m = 1:Nm
-        [sdraw, Ydraw] = simsmooth_HS(Y_o, Y_f, Y_l, Y_u, simdata.params, p_z, max_iter);
+        [sdraw, Ydraw] = simsmooth_HS(Y_o, Y_f, Y_l, Y_u, simdata.params, p_z, max_iter, model);
     end
     telapsed = toc; 
 end
