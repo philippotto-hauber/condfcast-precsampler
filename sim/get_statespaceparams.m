618c95ad2cca1ccbccf1bd6b306bf95e814b49ef
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-04-11 11:02:30 +0200

    set H = diag(1e-8)
    - this is necessary because for a VAR with conditional forecasting the
    measurement error covariance matrix F would otherwise be 0

--- sim/get_statespaceparams.m
+++ sim/get_statespaceparams.m
@@ -2,10 +2,10 @@ function [T, Z, H, R, Q, s0, P0] = get_statespaceparams(params, y, model)
 
 if strcmp(model, 'ssm')
     % generic state space model
-    T = params.params.phi; 
-    Z = params.params.lambda;
-    H = diag(params.params.sig_eps);
-    Q = params.params.sig_ups; 
+    T = params.phi; 
+    Z = params.lambda;
+    H = diag(params.sig_eps);
+    Q = params.sig_ups; 
     R = eye(size(T, 1)); 
     s0 = zeros(size(T, 1), 1); 
     P0 = 1 * eye(size(T, 1));
@@ -14,7 +14,7 @@ elseif strcmp(model, 'var')
     [T, R, Q] = var_companion(params.B, params.Sigma);
     Z = zeros(size(Q, 1), size(T, 2));
     Z(1:size(Q, 1), 1: size(Q, 1)) = eye(size(Q, 1));
-    H = zeros(size(Q, 1)); % 1e-8 * eye(size(Q, 1))
+    H = 1e-8 * eye(size(Q, 1)); % zeros(size(Q, 1))
     
     % s0 and P0
     Np = size(params.B, 2) / size(params.B, 1);
@@ -22,7 +22,7 @@ elseif strcmp(model, 'var')
     for p = 1:Np-1
         s0 = [s0; y(:, end-p)]; 
     end
-    P0 = zeros(size(s0, 1)); % 1e-8 * eye(size(T, 1))
+    P0 = zeros(size(s0, 1)); % 1e-8 * eye(size(T, 1))  
 end
    
 
