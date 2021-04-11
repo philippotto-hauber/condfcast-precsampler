3e561bfda2f5c9723f85b0fd50b6074ba4539d7e
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-04-11 11:59:07 +0200

    update analysis of sim output
    - convert ftypes, specs and samplers to factor with appropriate labels
    
    - free y scale in facet_wrap

--- sim/analyze_out.R
+++ sim/analyze_out.R
@@ -6,14 +6,27 @@ library(dplyr, tidyr)
 
 dir_in <- "./out/"
 
-dims = c("Nt_100_Nh_5_Nn_100_Ns_2",
-         "Nt_100_Nh_5_Nn_20_Ns_2",
-         "Nt_100_Nh_5_Nn_100_Ns_20",
-         "Nt_100_Nh_5_Nn_20_Ns_25")
+dims = c("Nt_100_Nh_5_Nn_20_Ns_2",
+         "Nt_100_Nh_5_Nn_100_Ns_2",
+         "Nt_100_Nh_5_Nn_100_Ns_10",
+         "Nt_100_Nh_5_Nn_3_Np_4",
+         "Nt_100_Nh_5_Nn_20_Np_4",
+         "Nt_100_Nh_5_Nn_100_Np_4")
+
+names_dims = c("small factor model",
+         "large factor model",
+         "large N,T factor model",
+         "small-sized VAR",
+         "medium-sized VAR",
+         "large-sized VAR")
 
 ftypes = c("uncond", "cond_hard", "cond_soft")
 
+names_ftypes <- c("unconditional forecasts", "conditional forecasts (hard)", "conditional forecasts (soft)")
+
 samplers <- c("CK", "DK", "HS")
+
+names_samplers <- c("Carter&Kohn (1994)", "Durbin&Koopman (2002)", "precision-sampler")
 
 file_prefix <- "runtime"
 
@@ -32,11 +45,23 @@ for (s in samplers){
   }
 }
 
+dat$spec <- factor(dat$spec, levels = dims, labels = names_dims)
+dat$ftype <- factor(dat$ftype, levels = ftypes, labels = names_ftypes)
+dat$sampler <- factor(dat$sampler, levels = samplers, labels = names_samplers)
 
 dat %>% 
   ggplot(aes(x = spec, y = value, color = sampler))+
     geom_boxplot()+
-    facet_wrap(~ftype, nrow = 1)+
+    facet_wrap(~ftype, ncol = 1, scales = "free_y")+
     theme(text = element_text(size=9),
-        axis.text.x = element_text(angle=90, hjust = 1)) 
+        axis.text.x = element_text(angle=90, hjust = 1),
+        legend.position="top") 
+
+dat %>% 
+  filter(sim == 1) %>%
+  ggplot(aes(x = spec, y = value, color = sampler))+
+  geom_point()+
+  facet_wrap(~ftype, ncol = 1, scales = "free_y")+
+  theme(text = element_text(size=9),
+        axis.text.x = element_text(angle=90, hjust = 1), legend.position="top") 
 
