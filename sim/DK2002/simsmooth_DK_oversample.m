952973b0da96cc5e874501782d9d5ae586f761cb
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-04-24 21:40:41 +0200

    update DK sim smoother
    - store K, L, invF on first run of Kalman smoother for subsequent runs
    
    - draw innovations using Rue&Held Alg 2.3

--- sim/DK2002/simsmooth_DK_oversample.m
+++ sim/DK2002/simsmooth_DK_oversample.m
@@ -13,18 +13,34 @@ Y = [Y_o, Y_f];
 % sample Ndraws
 store_Ydraw = NaN(Nn, Nh, Ndraws);
 
-for m = 1:Ndraws
+% first draw outside of loop
+[aplus, Yplus] = gen_aplusYplus(T, Z, H, R, Q, a1, P1, NtNh, Ns, Nn);
+Ystar = Y-Yplus;
+
+[ahatstar, K, L, invF] = kalmansmoother_storeKLinvF(Ystar, T, Z, H, R, Q, a1, P1);
+adraw = ahatstar + aplus ; % random draw of state vector
+Ydraw_tmp = Z * ahatstar(:, Nt+1:NtNh) + Yplus(:, Nt+1:NtNh); % this one is correct
+
+Ydraw = Y_f; % overwrite NaN's in Y_f with draws
+Ydraw(ind_fore) = Ydraw_tmp(ind_fore); 
+if any(isnan(Ydraw), 'all')
+    error('There should be no NaN in return arg Ydraw!')
+end
+store_Ydraw(:, :, 1) = Ydraw; 
+
+% loop 
+for m = 2:Ndraws
     % draw from joint distribution of a and Y
     [aplus, Yplus] = gen_aplusYplus(T, Z, H, R, Q, a1, P1, NtNh, Ns, Nn);
     Ystar = Y-Yplus;
 
     % run Kalman smoother
     a1 = zeros(size(a1)); % set E[alpha_1|Ystar_0] to 0. See Jarocinski (2015)
-    ahatstar = kalmansmoother(Ystar, T, Z, H, R, Q, a1, P1);
+    ahatstar = kalmansmoother(Ystar, T, Z, K, L, invF, R, Q, a1, P1);
 
     % draw a and Y_f
     adraw = ahatstar + aplus ; % random draw of state vector
-    Ydraw_tmp = Z * ahatstar(:, Nt+1:NtNh) + Yplus(:, Nt+1:NtNh); % this one is correct
+    Ydraw_tmp = Z * ahatstar(:, Nt+1:NtNh) + Yplus(:, Nt+1:NtNh); 
 
     % overwrite NaN's in Y_f with draws
     Ydraw = Y_f;
@@ -40,16 +56,24 @@ function [aplus, Yplus] = gen_aplusYplus(T, Z, H, R, Q, a1, P1, NtNh, Ns, Nn)
 aplus = NaN(Ns, NtNh+1);
 Yplus = NaN(Nn, NtNh);
 
-aplus(:,1) = mvnrnd(a1, P1)'; % initial values of state
+if rcond(P1)==0
+    aplus(:, 1) = a1;
+else
+    aplus(:,1) = mvnrnd(a1, P1)'; % initial values of state
+end
 
+cholH = chol(H, 'lower');
+cholQ = chol(Q, 'lower');
 for t=1:NtNh
-    Yplus(:, t) = Z * aplus(:, t)+ mvnrnd(zeros(size(Z, 1), 1), H)';
-    aplus(:, t+1) = T * aplus(:, t) + R * mvnrnd(zeros(size(Q, 1), 1), Q)';
+    %Yplus(:, t) = Z * aplus(:, t)+ mvnrnd(zeros(size(Z, 1), 1), H)';
+    Yplus(:, t) = Z * aplus(:, t)+ rue_held_alg2_3(zeros(size(Z, 1), 1), cholH);
+    %aplus(:, t+1) = T * aplus(:, t) + R * mvnrnd(zeros(size(Q, 1), 1), Q)';
+    aplus(:, t+1) = T * aplus(:, t) + R * rue_held_alg2_3(zeros(size(Q, 1), 1), cholQ);
 end
 
 aplus(:,end) = [];
 
-function ahatstar = kalmansmoother(Y, T, Z, H, R, Q, a1, P1)
+function [ahatstar, K, L, invF] = kalmansmoother_storeKLinvF(Y, T, Z, H, R, Q, a1, P1)
 
 [Nn, NtNh] = size(Y);
 Ns = size(T, 1); 
@@ -57,6 +81,7 @@ Ns = size(T, 1);
 % prepare Kalman filter
 v = cell(NtNh, 1);
 invF = cell(NtNh, 1);
+K = cell(NtNh, 1);
 L = NaN(Ns, Ns, NtNh);
 a = a1;
 P = P1;
@@ -72,10 +97,58 @@ for t=1:NtNh
     v{t,1} = Y(notmissing,t) - Wt*Z*a;
     F = Wt*Z*P*(Wt*Z)' + Wt*H*Wt';
     invF{t,1} = F\eye(size(F,1));
-    K = T*P*(Wt*Z)'*invF{t,1};
-    L(:,:,t) = T-K*Wt*Z; 
-    a = T*a + K*v{t,1};
+    K{t, 1} = T*P*(Wt*Z)'*invF{t,1};
+    L(:,:,t) = T-K{t, 1}*Wt*Z; 
+    a = T*a + K{t, 1}*v{t,1};
     P = T*P*L(:,:,t)'+RQR;
+end
+
+% prepare Kalman smoother to get r
+
+r = NaN(size(T,1), NtNh);
+r(:,end) = zeros(Ns, 1);
+
+% backward recursion 
+for t = (NtNh-1):-1:1
+    % check for missings 
+    notmissing = ~isnan(Y(:,t+1));
+    Wt = eye_N(notmissing,:);
+    r(:,t) = (Wt*Z)'*invF{t+1,1}*v{t+1,1}+L(:,:,t+1)'*r(:,t+1); 
+end
+
+% r0
+notmissing = ~isnan(Y(:,1));
+Wt = eye_N(notmissing,:);
+r_0 = (Wt*Z)'*invF{1,1}*v{1,1}+L(:,:,1)'*r(:,1);
+
+
+% forward recursions to get smoothed state alphahatstar
+ahatstar = NaN(Ns, NtNh);
+ahatstar(:, 1) = a1 + P1*r_0;
+
+for t=2:NtNh
+    ahatstar(:,t) = T*ahatstar(:,t-1) + RQR*r(:,t-1);
+end
+
+function [ahatstar, K, L, invF] = kalmansmoother(Y, T, Z, K, L, invF, R, Q, a1, P1)
+
+[Nn, NtNh] = size(Y);
+Ns = size(T, 1); 
+
+% prepare Kalman filter
+v = cell(NtNh, 1);
+a = a1;
+eye_N = eye(Nn);
+RQR = R*Q*R';
+
+% forward recursions
+for t=1:NtNh
+    % check for missings 
+    notmissing = ~isnan(Y(:,t));
+    Wt = eye_N(notmissing,:);
+    % proceed with recursions
+    v{t,1} = Y(notmissing,t) - Wt*Z*a;
+    a = T*a + K{t, 1}*v{t,1};
 end
 
 % prepare Kalman smoother to get r
