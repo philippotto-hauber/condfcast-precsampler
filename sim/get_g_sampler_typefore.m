4306a954db8144edb2ca51df219d001d911d4bcf
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-04-26 16:26:23 +0200

    change order of loop
    - samplers now run from 1:Ng, Ng+1:2*Ng, 2*Ng+1:3*Ng
    
    - this allows parallelization via independent batches for the different
    samplers

--- sim/get_g_sampler_typefore.m
+++ sim/get_g_sampler_typefore.m
@@ -1,16 +1,15 @@
 function [g, sampler, type_fore] = get_g_sampler_typefore(n)
 
-Ng = 3;
+Ng = 10;
 samplers = {'CK', 'DK', 'HS'};
 type_fores = {'uncond', 'cond_hard', 'cond_soft'};
 
 % calculate combinations of specifications
 Ng_tmp = repmat(1:Ng, 1, length(samplers) * length(type_fores)) ; 
-Nsamplers_tmp = repmat(kron(1:length(samplers), ones(1, length(1:Ng))), 1 , length(type_fores));
-Ntypes_tmp = kron(1:length(type_fores), ones(1, length(1:Ng) * length(samplers)));
+Ntypes_tmp = repmat(kron(1:length(type_fores), ones(1, length(1:Ng))), 1 , length(samplers));
+Nsamplers_tmp = kron(1:length(samplers), ones(1, length(1:Ng) * length(type_fores)));
 
 % back out specs for n
-
 g = Ng_tmp(n);
 switch Nsamplers_tmp(n)
     case 1
