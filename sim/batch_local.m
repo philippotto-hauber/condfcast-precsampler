4223f687f05f7e2c422ef4c74ad34a5edc76c3aa
Philipp Hauber <44675474+phauber@users.noreply.github.com>
2021-05-07 09:50:08 +0200

    add sim counter

--- sim/batch_local.m
+++ sim/batch_local.m
@@ -3,11 +3,13 @@ clear; close all; clc;
 Ng = 10;
 Nsampler = 3;
 Ntypes = 3;
+Nsim = Ng * Nsampler * Ntypes;
 for nsampler = 1:Nsampler
     tmp_sampler = (nsampler-1)*Ntypes*Ng;
     for ntype = 1:Ntypes
         tmp_type = (ntype-1) * Ng;
         for g = 1:Ng
+            disp(tmp_sampler + tmp_type + g)
             simul(tmp_sampler + tmp_type + g);
         end
     end
