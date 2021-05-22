02332c77f66417c1c98d50601464be763da9184d
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-05-22 07:25:49 +0200

    update construct_vintages.R
    - outsource helper functions to individual files
    
    - load list of vintages for which there are no missing forecasts

--- data/construct_vintages.R
+++ data/construct_vintages.R
@@ -1,125 +1,11 @@
 rm(list = ls())
 setwd("C:/Users/Philipp/Documents/GitHub/condfcast-precsampler/data")
 
-# PACKAGES----
-library(bundesbank)
-library(lubridate)
-library(dplyr)
-library(tidyr)
-library(readxl)
-library(readr)
+# SOURCE FUNCTIONS ----
+source("realtime_data.R") # spec details for real-time data from Bbk
+source("financial_data.R") # spec details for financial data from Bbk
+source("functions.R") # helper functions
 
-# SOURCED FUNCTIONS ----
-source("realtime_data.R")
-source("financial_data.R")
-
-# HELPER FUNCTIONS ----
-download_realtime_data <- function(code, v_star)
-{
-  print(code)
-  dat <- getSeries(code,
-                   start = "1991-01",
-                   end = format(Sys.Date(), "%Y-%m"),
-                   return.class = "data.frame",
-                   verbose = F, dest.dir = NULL)
-  
-  
-  # convert dates and vintages to date format
-  tmp <- rownames(dat)
-  yy <- as.numeric(substr(tmp, 1, 4))
-  mm <- as.numeric(substr(tmp, 6, 7)) 
-  
-  date <- make_date(year = yy, month = mm)
-  
-  tmp <- colnames(dat)
-  yy <- as.numeric(substr(tmp, 1, 4))
-  mm <- as.numeric(substr(tmp, 6, 7)) 
-  dd <- as.numeric(substr(tmp, 9, 10))
-  
-  vint <- make_date(year = yy, month = mm, day = dd)
-  
-  # select vintage
-  x_vint <- dat[, sum(vint <= v_star), drop = T] 
-  
-  return(data.frame(value = x_vint, date = date))
-}
-
-download_financial_data <- function(code)
-{
-  print(code)
-  dat <- getSeries(code,
-                   start = "1991-01",
-                   end = format(Sys.Date(), "%Y-%m"),
-                   return.class = "data.frame",
-                   verbose = F, dest.dir = NULL)
-  
-  
-  # convert dates and vintages to date format
-  tmp <- dat$dates
-  yy <- as.numeric(substr(tmp, 1, 4))
-  mm <- as.numeric(substr(tmp, 6, 7)) 
-  
-  date <- make_date(year = yy, month = mm)
-
-  return(data.frame(value = dat$values, date = date))
-}
-
-# helper functions
-export_vintage_to_csv <- function(df, sample_start, name, dir_out)
-{
-  df %>% 
-    filter(date >= sample_start) %>%
-    select(date, trafo, mnemonic) %>%
-    pivot_wider(names_from = mnemonic, values_from = trafo) -> df
-  
-  ind_vars <- which(colnames(df)!= "date") # col indices
-  ind_sample <- min(min(which(apply(is.na(df[, ind_vars]), 1, sum) == length(ind_vars)))-1, nrow(df)) # row indices
-  
-  df_export <- df[seq(1, ind_sample), ]
-  
-  write.csv(df_export, paste0(dir_out, name), row.names = F, na = "NaN")
-  
-  return(df_export)
-}
-
-transform_series <- function(df, trafo_code)
-{
-  logdiff_series <- function(x){c(NA, 100 * log(x[seq(2, length(x))] / x[seq(1, length(x)-1)]))}
-  diff_series <- function(x){c(NA, x[seq(2, length(x))] - x[seq(1, length(x)-1)])}
-  if (trafo_code == "log, diff"){
-    df %>% mutate(trafo = logdiff_series(raw)) -> df
-  } else if (trafo_code == "diff"){
-    df %>% mutate(trafo =  diff_series(raw)) -> df
-  }else if (trafo_code == "none"){
-    df %>% mutate(trafo =  raw) -> df
-  }
-  return(df)
-}
-
-aggregate_to_Q <- function(df, freq_code)
-{
-  if (freq_code == "M")
-  {
-    df %>% 
-      mutate(yy = year(date),
-             mm = month(date),
-             qq = ceiling(mm/3)) %>%
-      group_by(yy, qq) %>%
-      summarize(raw = mean(value, na.rm = T)) %>%
-      ungroup() %>%
-      mutate(date = make_date(year = yy, month = qq * 3, day = 1L)) -> df
-  } else
-  {
-    df %>% 
-      mutate(yy = year(date),
-             mm = month(date),
-             qq = ceiling(mm/3)) %>%
-      mutate(date = make_date(year = yy, month = qq * 3, day = 1L)) %>%
-      rename(raw = value) -> df
-  }
-  
-  return(df)
-}
 
 # MAIN FUNCTIONS----
 get_realtime_data <- function(df_in, v_star)
@@ -265,15 +151,40 @@ get_survey_data <- function(v_star)
   return(df_out)
 }
 
+export_vintage_to_csv <- function(df, sample_start, name, dir_out)
+{
+  df %>% 
+    filter(date >= sample_start) %>%
+    select(date, trafo, mnemonic) %>%
+    pivot_wider(names_from = mnemonic, values_from = trafo) -> df
+  
+  ind_vars <- which(colnames(df)!= "date") # col indices
+  ind_sample <- min(min(which(apply(is.na(df[, ind_vars]), 1, sum) == length(ind_vars)))-1, nrow(df)) # row indices
+  
+  df_export <- df[seq(1, ind_sample), ]
+  
+  write.csv(df_export, paste0(dir_out, name), row.names = F, na = "NaN")
+  
+  return(df_export)
+}
+
+# PACKAGES----
+library(bundesbank)
+library(lubridate)
+library(dplyr)
+library(tidyr)
+library(readxl)
+library(readr)
+
 # SET-UP----
 dir_out <- paste0(getwd(), "/vintages/")
 realtime_data_spec <- realtime_data()
 financial_data_spec <- financial_data()
-vintages <- c("2006-04-05")
+load("list_vintages.Rda")
 sample_start <- "1996-01-01"
 
 # LOOP OVER VINTAGES----
-for (v_star in vintages)
+for (v_star in list_vintages)
 {
     
     df_data <- data.frame()
