6ec774205b2d8f035562a00d8fafea7061cf479d
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-05-26 19:32:04 +0200

    update function to gen fore from dfm

--- models/dfm/fore_dfm.m
+++ models/dfm/fore_dfm.m
@@ -1,14 +1,12 @@
-function fore_dfm(spec)
+function fore_dfm(n_spec)
 
-addpath('../../functions')
-dir_in = 'C:\Users\Philipp\Documents\Dissertation\condfcast-precsampler\models\dfm\';
-dir_out = '';
+dir_in = 'draws/';
+dir_out = 'forecasts/';
 % vintage and forecast type
-[v, forecast_type] = get_v_forecast_type(spec);
+[v, model_spec, forecast_type] = get_specs_fore(n_spec);
 
 % load estim output
-load([dir_in, 'out_dfm_', v, '.mat']);
-
+load([dir_in, 'out_dfm_', model_spec, '_', v, '.mat']);
 
 if strcmp(forecast_type, 'unconditional')
 % unconditional forecasts
@@ -19,9 +17,8 @@ Nh = size(y_f, 1);
 Nn = size(y_f, 2); 
 p_z = p_timet([y_o; y_f]', out_dfm.options.Nr);
 store_Y_fore = NaN(Nh * Ndraws, Nn);
-
 for m = 1:Ndraws
-    [f, Yplus] = f_simsmoothsHS(y_o, y_f, out_dfm.draws.phi(:, :, m),  eye(out_dfm.options.Nr), out_dfm.draws.lam(:, :, m), out_dfm.draws.psi(:, :, m), out_dfm.draws.sig2(:, m), p_z, 1);
+    [~, Yplus] = f_simsmoothsHS(y_o, y_f, out_dfm.draws.phi(:, :, m),  eye(out_dfm.options.Nr), out_dfm.draws.lam(:, :, m), out_dfm.draws.psi(:, :, m), out_dfm.draws.sig2(:, m), p_z, 1);
     store_Y_fore(Nh*(m-1)+1:Nh*m, :) = Yplus(end-Nh+1:end, :);    
 end
 
@@ -29,11 +26,6 @@ count_hs = kron(ones(Ndraws, 1), (1:Nh)');
 count_draws = kron((1:Ndraws)', ones(Nh, 1));
 % restandardize
 store_Y_fore = store_Y_fore .* out_dfm.std_y_o + out_dfm.mean_y_o;
-
-% export to csv
-out_fore = [count_hs, count_draws, store_Y_fore];
-writetable(array2table(out_fore, 'VariableNames',[{'horizon', 'draw'}, out_dfm.mnemonics]), [dir_out 'uncond_' v '.csv'])
-
 
 elseif strcmp(forecast_type, 'conditional (hard)')
 % conditional forecasts
@@ -43,19 +35,16 @@ Ndraws = size(out_dfm.draws.lam, 3);
 Nh = size(y_f, 1);
 Nn = size(y_f, 2); 
 p_z = p_timet([y_o; y_f]', out_dfm.options.Nr);
-store_Y_fore = NaN(Nh * Ndraws, Nn);for m = 1:size(out_dfm.draws.lam, 3)
-    [f, Yplus] = f_simsmoothsHS(y_o, y_f, out_dfm.draws.phi(:, :, m),  eye(out_dfm.options.Nr), out_dfm.draws.lam(:, :, m), out_dfm.draws.psi(:, :, m), out_dfm.draws.sig2(:, m), p_z, 1);
-    store_Y_fore(:, :, m) = Yplus(end-size(y_f, 1)+1:end, :);    
+store_Y_fore = NaN(Nh * Ndraws, Nn);
+for m = 1:Ndraws
+    [~, Yplus] = f_simsmoothsHS(y_o, y_f, out_dfm.draws.phi(:, :, m),  eye(out_dfm.options.Nr), out_dfm.draws.lam(:, :, m), out_dfm.draws.psi(:, :, m), out_dfm.draws.sig2(:, m), p_z, 1);
+    store_Y_fore(Nh*(m-1)+1:Nh*m, :) = Yplus(end-size(y_f, 1)+1:end, :);    
 end
 
 count_hs = kron(ones(Ndraws, 1), (1:Nh)');
 count_draws = kron((1:Ndraws)', ones(Nh, 1));
 % restandardize
 store_Y_fore = store_Y_fore .* out_dfm.std_y_o + out_dfm.mean_y_o;
-
-% export to csv
-out_fore = [count_hs, count_draws, store_Y_fore];
-writetable(array2table(out_fore, 'VariableNames',[{'horizon', 'draw'}, out_dfm.mnemonics]), [ dir_out 'hardcond_' v '.csv'])
 
 elseif strcmp(forecast_type, 'conditional (soft)')
 % soft conditioning
@@ -64,38 +53,42 @@ y_f = NaN(size(out_dfm.y_c));
 Ndraws = size(out_dfm.draws.lam, 3);
 Nh = size(y_f, 1);
 Nn = size(y_f, 2); 
-p_z = p_timet([y_o; y_f]', Nr);
+p_z = p_timet([y_o; y_f]', out_dfm.options.Nr);
 y_l = out_dfm.y_l;
 y_u = out_dfm.y_u;
 ind_cond = not(isnan(y_l));
-store_Y_fore_c_soft = NaN(size(y_f, 1), size(y_o, 2), size(out_dfm.draws.lam, 3));
+store_Y_fore = NaN(Nh * Ndraws, Nn);
 Nsample = 1000;
-counter = 1;
+m = 1;
 iter_max = 10000;
 iter = 1;
-while counter < Ndraws && iter < iter_max
-    disp(counter)
-    [~, Yplus] = f_simsmoothsHS(y_o, y_f, out_dfm.draws.phi(:, :, m),  eye(Nr), out_dfm.draws.lam(:, :, m), out_dfm.draws.psi(:, :, m), out_dfm.draws.sig2(:, m), p_z, Nsample);
+while m < Ndraws && iter < iter_max
+    [~, Yplus] = f_simsmoothsHS(y_o, y_f, out_dfm.draws.phi(:, :, m),  eye(out_dfm.options.Nr), out_dfm.draws.lam(:, :, m), out_dfm.draws.psi(:, :, m), out_dfm.draws.sig2(:, m), p_z, Nsample);
     % check conditions    
     for m1 = 1:Nsample
         Yplus_tmp = Yplus(end-size(y_f, 1)+1:end, :, m1); 
         check_u = Yplus_tmp(ind_cond) < y_u(ind_cond);
         check_l= Yplus_tmp(ind_cond) > y_l(ind_cond);
         if  all(check_u) && all(check_l)
-            store_Y_fore_c_soft(:, :, counter) = Yplus_tmp;
-            counter = counter + 1;
+            store_Y_fore(Nh*(m-1)+1:Nh*m, :) = Yplus_tmp;
+            m = m + 1;
         end
     end
     iter = iter + 1; 
+end
+
+if iter == iter_max
+    error('Soft conditioning took too many iterations. Abort')
 end
 
 count_hs = kron(ones(Ndraws, 1), (1:Nh)');
 count_draws = kron((1:Ndraws)', ones(Nh, 1));
 % restandardize
 store_Y_fore = store_Y_fore .* out_dfm.std_y_o + out_dfm.mean_y_o;
+end
 
 % export to csv
 out_fore = [count_hs, count_draws, store_Y_fore];
-writetable(array2table(out_fore, 'VariableNames',[{'horizon', 'draw'}, out_dfm.mnemonics]), [dir_out 'softcond_' v '.csv'])
-end
+writetable(array2table(out_fore, 'VariableNames',[{'horizon', 'draw'}, out_dfm.mnemonics]), [dir_out,  forecast_type, '_', model_spec, v '.csv'])
+
 
--- models/dfm/get_specs_fore.m
+++ models/dfm/get_specs_fore.m
@@ -2,7 +2,7 @@ function [v, model_spec, forecast_type] = get_specs_fore(n_spec)
 
 % read in list of vintages
 dir_in = '../../data/';
-vintages = readmatrix([dir_in, 'list_vintages.csv']);
+vintages = importdata([dir_in, 'list_vintages.csv']);
 
 % read in model spec file
 dir_in = '';
@@ -11,9 +11,16 @@ model_specs = readmatrix([dir_in, 'model_specs.csv']);
 % forecast_types
 forecast_types = {'unconditional', 'conditional (hard)', 'conditional (soft)'};
 
-rep(1:length(vintages), length(model_specs));
-
-
+ind_vs = repmat(1:length(vintages), 1, size(model_specs, 1) * length(forecast_types));
+ind_types = repmat(kron(1:length(forecast_types), ones(1, length(vintages))), 1, size(model_specs, 1));
+ind_specs = kron(1:size(model_specs, 1), ones(1, length(vintages) * length(forecast_types)));
+ 
+v = vintages{ind_vs(n_spec)};
+forecast_type = forecast_types{ind_types(n_spec)};
+model_spec = ['Nr' num2str(model_specs(ind_specs(n_spec), 1)), ...
+             '_Nj' num2str(model_specs(ind_specs(n_spec), 2)), ...
+             '_Np' num2str(model_specs(ind_specs(n_spec), 3)), ...
+             '_Ns' num2str(model_specs(ind_specs(n_spec), 4))];
 
 
 
