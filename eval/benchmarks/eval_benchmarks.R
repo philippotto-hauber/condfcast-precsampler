5a5c65e361315de474d3fcad07708cf50611bd40
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-06-04 18:33:24 +0200

    source functions from file, add sections

--- eval/benchmarks/eval_benchmarks.R
+++ eval/benchmarks/eval_benchmarks.R
@@ -14,24 +14,9 @@ dir_releases <- "./../releases/"
 list_vintages <- read.csv(paste0(dir_vintages, "list_vintages.csv"), header=F)
 
 # Functions----
+source("./../../functions/functions.R")
 
-# wrapper for logS_sample
-wrap_logs_sample <- function(y, dat){
-  logs <- scoringRules::logs_sample(y[1], dat)
-  return(logs)
-}
-
-# wrapper for crps_sample
-wrap_crps_sample <- function(y, dat){
-  crps <- scoringRules::crps_sample(y[1], dat)
-  return(crps)
-}
-
-determine_horizon <- function(quarter, vintage)
-{
-  floor(lubridate::day(lubridate::days(quarter)-lubridate::days(vintage))/90)
-}
-
+# Read in benchmark forecasts for each vintage----
 df_fore <- data.frame()
 for (v in list_vintages[, 1])
 #for (v in list_vintages[1, 1])
@@ -45,6 +30,9 @@ df_fore$quarter <- as.Date(df_fore$quarter)
 
 df_fore$vintage <- as.Date(df_fore$vintage)
 
+
+# Merge with realizations and calculate log score and CRPS----
+
 load(paste0(dir_releases, "releases.Rda"))
 
 df_releases <- select(df_releases, quarter, mnemonic, realization = value)
@@ -55,9 +43,9 @@ df <- merge(df_fore, df_releases,
 
 rm(df_fore, df_releases)
 
-df <- filter(df, series != "inv")
+# remove inventories as they were causing Inf when calculating the log score
+df <- filter(df, series != "inv") 
 
-# calculate log score and crps
 df %>% 
   group_by(series, quarter, vintage) %>% 
   summarise(logs = wrap_logs_sample(realization, value),
@@ -73,6 +61,8 @@ df_eval_tmp %>%
             mean_crps = mean(crps),
             n_quarters = n()) -> df_eval
 
+
+# Output file as Rda----
 save(file = "df_benchmark.Rda", df_eval)
 
 
